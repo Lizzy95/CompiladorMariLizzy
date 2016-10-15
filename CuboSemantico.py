7a12b257ac92ac95787ba4bdcb348c04226c01a1
Lizzy95 <lizzi16.gd95@gmail.com>
2016-10-15 16:39:38 -0500

    todoooo

---
+++ CuboSemantico.py
@@ -0,0 +1,125 @@
+#1 +
+#2 -
+#3 *
+#4 /
+#5 =
+#6 >
+#7 <
+#8 >=
+#9 <=
+#10 ==
+#11 !=
+#12 OR
+#13 &&
+
+#enteros
+cuboSemantico = [[[-1 for k in range(13)] for j in range(9)] for i in range(9)]
+
+
+#entero
+cuboSemantico[1][1][1]= 1
+cuboSemantico[1][1][2]= 1
+cuboSemantico[1][1][3]= 1
+cuboSemantico[1][1][4]= 1
+cuboSemantico[1][1][5]= 1
+cuboSemantico[1][1][6]= 8
+cuboSemantico[1][1][7]= 8
+cuboSemantico[1][1][8]= 8
+cuboSemantico[1][1][9]= 8
+cuboSemantico[1][1][10]= 8
+cuboSemantico[1][1][11]= 8
+
+cuboSemantico[1][2][1]= 7
+cuboSemantico[1][2][2]= 7
+cuboSemantico[1][2][3]= 7
+cuboSemantico[1][2][5]= 2
+cuboSemantico[1][2][6]= 8
+cuboSemantico[1][2][7]= 8
+cuboSemantico[1][2][8]= 8
+cuboSemantico[1][2][9]= 8
+cuboSemantico[1][2][10]= 8
+cuboSemantico[1][2][11]= 8
+
+
+#cuadrado
+cuboSemantico[2][2][1]= 2
+cuboSemantico[2][2][2]= 2
+cuboSemantico[2][2][3]= 2
+cuboSemantico[2][2][4]= 2
+cuboSemantico[2][2][5]= 2
+
+
+
+
+
+#rectangulo
+cuboSemantico[3][3][1]= 3
+cuboSemantico[3][3][2]= 3
+cuboSemantico[3][3][3]= 3
+cuboSemantico[3][3][4]= 3
+cuboSemantico[3][3][5]= 3
+
+#circulo
+cuboSemantico[4][4][1]= 4
+cuboSemantico[4][4][2]= 4
+cuboSemantico[4][4][3]= 4
+cuboSemantico[4][4][4]= 4
+cuboSemantico[4][4][5]= 4
+
+
+#linea
+cuboSemantico[5][5][1]= 5
+cuboSemantico[5][5][2]= 5
+cuboSemantico[5][5][3]= 5
+cuboSemantico[5][5][4]= 5
+cuboSemantico[5][5][5]= 5
+
+
+#estrella
+cuboSemantico[6][6][1]= 6
+cuboSemantico[6][6][2]= 6
+cuboSemantico[6][6][3]= 6
+cuboSemantico[6][6][4]= 6
+cuboSemantico[6][6][5]= 6
+
+
+#decimal
+cuboSemantico[7][7][1]= 7
+cuboSemantico[7][7][2]= 7
+cuboSemantico[7][7][3]= 7
+cuboSemantico[7][7][4]= 7
+cuboSemantico[7][7][5]= 7
+cuboSemantico[7][7][6]= 7
+cuboSemantico[7][7][7]= 7
+cuboSemantico[7][7][8]= 7
+cuboSemantico[7][7][9]= 7
+cuboSemantico[7][7][10]= 7
+cuboSemantico[7][7][11]= 8
+
+cuboSemantico[2][1][1]= 7
+cuboSemantico[2][1][2]= 7
+cuboSemantico[2][1][3]= 7
+cuboSemantico[2][1][5]= 2
+cuboSemantico[2][1][6]= 8
+cuboSemantico[2][1][7]= 8
+cuboSemantico[2][1][8]= 8
+cuboSemantico[2][1][9]= 8
+cuboSemantico[2][1][10]= 8
+cuboSemantico[2][1][11]= 8
+
+
+
+
+#bool
+cuboSemantico[8][8][5]= 8
+cuboSemantico[8][8][6]= 8
+cuboSemantico[8][8][7]= 8
+cuboSemantico[8][8][8]= 8
+cuboSemantico[8][8][9]= 8
+cuboSemantico[8][8][10]= 8
+cuboSemantico[8][8][11]= 8
+
+
+
+
+
---
+++ TFunc.py
@@ -0,0 +1,5 @@
+class TFunc:
+	def __init__(self, nombre, tipo, diccvars):
+		self.nombre = nombre
+		self.tipo = tipo
+		self.diccvars = {}
